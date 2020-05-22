75f211f734facfd6cf687d89568c0a02fe47d97a
Fredmanzi <manzifred13@gmail.com>
2020-05-22 09:30:18 +0300

    Initial commit

---
+++ lib/models/activity_model.dart
@@ -0,0 +1,17 @@
+class Activity {
+  final String imageUrl;
+  final String name;
+  final String type;
+  final List<String> startTimes;
+  final int rating;
+  final int price;
+
+  Activity({
+    this.imageUrl,
+    this.name,
+    this.type,
+    this.startTimes,
+    this.rating,
+    this.price,
+  });
+}
