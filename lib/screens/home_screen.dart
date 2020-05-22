8bab61afd52b0690caf79916c21e90b26d8a2e92
Fredmanzi <manzifred13@gmail.com>
2020-05-22 11:21:26 +0300

    add row for destinations

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
   List<IconData> _icons = [
     FontAwesomeIcons.plane,
     FontAwesomeIcons.bed,
@@ -15,17 +16,28 @@ class _HomeScreenState extends State<HomeScreen> {
     FontAwesomeIcons.biking
   ];
   Widget _buildIcon(int index) {
-    return Container(
-      height: 60.0,
-      width: 60.0,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30.0),
-        color: Theme.of(context).accentColor,
-      ),
-      child: Icon(
-        _icons[index],
-        size: 25.0,
-        color: Theme.of(context).primaryColor,
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: _selectedIndex == index
+              ? Theme.of(context).accentColor
+              : Color(0xFFE7EBEE),
+        ),
+        child: Icon(
+          _icons[index],
+          size: 20.0,
+          color: _selectedIndex == index
+              ? Theme.of(context).primaryColor
+              : Color(0xFFB4C1C4),
+        ),
       ),
     );
   }
@@ -35,25 +47,56 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SafeArea(
           child: ListView(
-            padding: EdgeInsets.symmetric(vertical: 30.0),
+        padding: EdgeInsets.symmetric(vertical: 30.0),
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(left: 20.0, right: 120.0),
+            padding: EdgeInsets.only(left: 20.0, right: 80.0),
             child: Text(
               'What would do you like to find?',
               style: TextStyle(
-                  fontSize: 30.0,
+                  fontSize: 20.0,
                   fontFamily: 'Poppins',
                   fontWeight: FontWeight.bold),
             ),
           ),
           SizedBox(height: 20.0),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: _icons
                   .asMap()
                   .entries
-                  .map((MapEntry map) => _buildIcon(map.key),).toList()
+                  .map(
+                    (MapEntry map) => _buildIcon(map.key),
+                  )
+                  .toList()),
+          Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Top Destinations',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+//                    letterSpacing: 1.0
+                      ),
+                    ),
+                    Text(
+                      'See all',
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600),
+
+                    )
+                  ],
+                ),
+              )
+            ],
           )
         ],
       )),
